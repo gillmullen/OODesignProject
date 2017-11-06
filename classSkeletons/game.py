8bb461c0861453d919ea4f1d61eb1c28abf99eb7
xCiaraG <ciara.godwin3@mail.dcu.ie>
2017-11-06 13:22:19 +0000

    Add class skeletons

---
+++ classSkeletons/game.py
@@ -0,0 +1,52 @@
+class Game:
+	def __init__(self, user1, user2, size):
+		'''Initialises a game
+		   user1: First user (User)
+		   user2: Second user (User)
+		   size: Size of board required (int)
+		   return None
+		'''
+		pass
+
+	def determine_whose_move(self):
+		'''Determines whose move is next
+		   return None
+		'''
+		pass
+
+	def check_game_state(self):
+		'''Checks if game has ended
+		   return None
+		'''
+		pass
+
+	def user_play(self, user, command):
+		'''Determines which play the user wants to make
+		   eg. Forfeit game, make move, pass turn
+		   user: User making command (User)
+		   command: Play user wants to make (list)
+		   return None
+		'''
+		pass
+
+	def make_move(self, user, move):
+		'''Makes players move
+		   user: User making move (User)
+		   move: Players chosen move ((int, int))
+		   return None 
+		'''
+		pass
+
+	def pass_turn(self, user):
+		'''Passes users turn
+		   user: User passing turn (User)
+		   return None
+		'''
+		pass
+
+	def forfeit_game(self, user):
+		'''Forfeits user from game
+		   user: User forfeiting game (User)
+		   return None
+		'''
+		pass
---
+++ classSkeletons/server.py
@@ -0,0 +1,70 @@
+class Server:
+	def __init__(self, user_list=[], wait_list=[]):
+		'''Initialises server
+		   user_list: Contains users known to server
+		   wait_list: Contains users waiting for a game
+		   return None
+		'''
+		pass
+
+	def _add_user(self, username):
+		'''Adds new user to system
+		   username: Username the user chooses (str)
+		   return None
+		'''
+		pass
+
+	def join_server(self, username):
+		'''Logs user onto server, creating a new account if necessary
+		   username: Username the user chooses (str)
+		   return None
+		'''
+		pass
+
+	def join_game(self, username, size):
+		'''Adds user to game
+		   username: Username for user (str)
+		   size: Size user wants for game (int)
+		   return player number and piece (str)
+		'''
+		pass 
+
+	def generate_game(self, user1, user2, size=9):
+		'''Generates a game for two users
+		   user1: First user (User)
+		   user2: Second user (User)
+		   size: Size of board required for game (int)
+		   return None
+		'''
+		pass
+
+	def check_wait_list(self):
+		'''Checks wait list. If there's more than one user in
+		   the wait list generate a game
+		   return None
+		'''
+		pass
+
+	def request_move(self, user):
+		'''Requests a user to make their next move
+		   user: User to make move (User)
+		   return None
+		'''
+		pass
+
+	def user_play(self, user, command):
+		'''Calls the Game with the users command
+		   user: User making command (User)
+		   command: Play user wants to make (list)
+		   return bool determining if an error occured
+		'''
+		pass
+
+	def _game_over(self, game, user1, user2):
+		'''Game has ended. Increments win_count for winner.
+		   game: Game which has ended (Game)
+		   user1: First user (User)
+		   user2: Second user (User)
+		   return win/lose message (str)
+		'''
+		pass
